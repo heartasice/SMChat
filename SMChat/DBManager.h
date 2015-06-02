93297d51ef20662f6b18fe7952a99200d81d5557
heartasice <Eric@Erics-MacBook-Air.local>
2015-06-02 22:01:21 +0800

    init
    init

---
+++ SMChat/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  SMChat
+//
+//  Created by Eric Che on 2/6/15.
+//  Copyright (c) 2015 Eric Che. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ SMChat/DBManager.m
@@ -0,0 +1,14 @@
+//
+//  DBManager.m
+//  SMChat
+//
+//  Created by Eric Che on 2/6/15.
+//  Copyright (c) 2015 Eric Che. All rights reserved.
+//
+
+#import "DBManager.h"
+#import "FMDB.h"
+#define DATA_BASE_PATH [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0]stringByAppendingString:@"/SMChat.db"]
+@implementation DBManager
+
+@end
